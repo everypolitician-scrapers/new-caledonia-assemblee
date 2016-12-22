59f886e82ffda2a597c598f1e71d5ed8d33add7a
Tony Bowden <tony@tmtm.com>
2016-12-22 12:54:09 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,24 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
 require 'open-uri'
-
 require 'pry'
+
 # require 'open-uri/cached'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_list(url)
@@ -29,20 +30,20 @@ def scrape_list(url)
     given_name = tds[1].text.tidy
     datatab = noko.css(tds[4].css('a/@href').text)
 
-    data = { 
-      id: datatab.css('img/@class').text[/wp-image-(\d+)/, 1],
-      name: "#{given_name} #{family_name}",
-      sort_name: "#{family_name}, #{given_name}",
+    data = {
+      id:          datatab.css('img/@class').text[/wp-image-(\d+)/, 1],
+      name:        "#{given_name} #{family_name}",
+      sort_name:   "#{family_name}, #{given_name}",
       family_name: family_name,
-      given_name: given_name,
-      area: tds[2].text.tidy,
-      party: tds[3].text.tidy,
-      image: datatab.css('img/@src').text,
-      term: '4',
-      source: url,
+      given_name:  given_name,
+      area:        tds[2].text.tidy,
+      party:       tds[3].text.tidy,
+      image:       datatab.css('img/@src').text,
+      term:        '4',
+      source:      url,
     }
     data[:image] = '' if data[:image].include?('no-elu.jpg') || data[:image].include?('img-elus.jpg')
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
